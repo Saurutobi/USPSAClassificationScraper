2e8e3f3cc3551e692127204098984c0312cf1547
Shawn Collinge <shawncollinge@gmail.com>
2022-08-08 03:38:40 +0000

    Refactored

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 
-INPUT_FILE_NAME = "Book1.csv"
-OUTPUT_FILE_NAME = "out.csv"
+INPUT_FILE_NAME = "uspsa.csv"
+OUTPUT_FILE_NAME = "out4.csv"
 SLEEP_TIME = 10 # in s between member searches
 WRITE_TO_CSV = 10 # write to csv every n pass
 
 
-StartingPrefix = ["A", "FY", "TY", "L", "B"]
+StartingPrefix = ["A", "FY", "TY"]
 Divisions = ["Open", "Limited", "Limited 10", "Production", "Revolver", "Single Stack", "Carry Optics", "PCC"]
 
 DictFromCSV = pd.read_csv(INPUT_FILE_NAME).to_dict()
@@ -35,31 +35,33 @@ for i in DictFromCSV['firstName']:
         OriginalPrefix = id.strip(StrippedID).upper()
         StartingIndex = 0
 
-        while not FoundUser and StartingIndex < len(StartingPrefix):
+        while not FoundUser and StartingIndex < (len(StartingPrefix) + 1):
             Response = requests.get(f"https://uspsa.org/classification/{id}")
             MySoup = BeautifulSoup(Response.text, "html.parser") 
 
             Error = MySoup.find(name="span", text="Error")    
             if Error is not None:
-                print("Rate limited saved current dict to csv, sleeping for 1 hour")
                 df = pd.DataFrame(CompleteDict)
                 df.to_csv(OUTPUT_FILE_NAME, index=False)
-                time.sleep(3600)
+                input(f"Rate limited at index {i} press enter to continue")
 
             else:
                 Error = MySoup.find(name="strong", text=re.compile("Oops!"))
                 FoundUser = Error is None
-                if not FoundUser:
+                if not FoundUser and StartingIndex < len(StartingPrefix):
+                    time.sleep(SLEEP_TIME)
                     if StartingPrefix[StartingIndex] == OriginalPrefix:
                         StartingIndex += 1
                     if StartingIndex < len(StartingPrefix):
                         id = f"{StartingPrefix[StartingIndex]}{StrippedID}"
-                        StartingIndex += 1
+                StartingIndex += 1
 
     if not FoundUser:
         print(f"Unable to find data for {FirstName} {LastName} at index {i}")
 
+
     else:
+        Unclassified = True
         for Division in Divisions:
             Results = MySoup.find(name="th", text=re.compile(Division))
             RawDivision = Results.getText()
@@ -67,7 +69,18 @@ for i in DictFromCSV['firstName']:
             RawClass = Results.find_next_sibling("td")
             RealClass = RawClass.getText()[-2:]
             Classification = RealClass.strip()
-            if Classification != "U":
+            if Classification == "X":
+                if Unclassified:
+                    Unclassified = False
+                    CompleteDict['firstName'][Index] = FirstName
+                    CompleteDict['lastName'][Index] = LastName
+                    CompleteDict['uspsaNumber'][Index] = id
+                    CompleteDict['division'][Index] = "Non-member"
+                    CompleteDict['classification'][Index] = "X"    
+                    Index += 1   
+
+            elif Classification != "U":
+                Unclassified = False
                 CompleteDict['firstName'][Index] = FirstName
                 CompleteDict['lastName'][Index] = LastName
                 CompleteDict['uspsaNumber'][Index] = id
@@ -75,12 +88,19 @@ for i in DictFromCSV['firstName']:
                 CompleteDict['classification'][Index] = Classification
                 Index += 1
         
-        if i % WRITE_TO_CSV == 0:
-            df = pd.DataFrame(CompleteDict)
-            df.to_csv(OUTPUT_FILE_NAME, index=False)
+        if Unclassified:
+            CompleteDict['firstName'][Index] = FirstName
+            CompleteDict['lastName'][Index] = LastName
+            CompleteDict['uspsaNumber'][Index] = id
+            CompleteDict['division'][Index] = "Unclassified"
+            CompleteDict['classification'][Index] = "U"    
+            Index += 1    
 
-        time.sleep(SLEEP_TIME)
+    if i % WRITE_TO_CSV == 0:
+        df = pd.DataFrame(CompleteDict)
+        df.to_csv(OUTPUT_FILE_NAME, index=False)
+
+    time.sleep(SLEEP_TIME)
 
 df = pd.DataFrame(CompleteDict)
 df.to_csv(OUTPUT_FILE_NAME, index=False)
-print("Done!")
